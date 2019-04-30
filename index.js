7eee03ae7380f5690f54a6d6df46fa1d687dd733
Mariana <maprimante@gmail.com>
2019-04-30 20:40:53 -0300

    npm publish

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const isNumberValid = (cardNum) => !isNaN(parseFloat(cardNum)) && isFinite(cardNum);
 
 const sumNumbers = (cardNum) => {
-  const dig = cardNum.split("");
+  const dig = cardNum.toString().split("");
   const stringNumber = dig.map(num => parseInt(num));
   const indLuhn = stringNumber.map((num, ind) => ((ind % 2 === 0) === true) ? num * 2 : num);
   const sumNumber = indLuhn.map((num) => num > 9 ? (num % 10) + 1 : num);
@@ -9,6 +9,9 @@ const sumNumbers = (cardNum) => {
   return sumTotal;
 };
 
-const cardValidator = (cardNum) => (cardNum.length === 16) && isNumberValid(cardNum) && (sumNumbers(cardNum) % 10 === 0);
+const cardValidator = (cardNum) => {
+  const cardNumString = cardNum.toString();
+  return (cardNumString.length === 16) && isNumberValid(cardNumString) && (sumNumbers(cardNum) % 10 === 0);
+};
 
 module.exports.cardValidator = cardValidator;
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,20 +1,26 @@
-const lib = require("../lib/index");
+const lib = require("../index");
 const expect = require("chai").expect;
 
 describe("cardValidator", () => {
-  it("valid", () => {
+  it("valid: '5155901285384518'", () => {
     expect(lib.cardValidator("5155901285384518")).to.be.equal(true);
   });
-  it("valid", () => {
+  it("valid: '1234567891234563'", () => {
     expect(lib.cardValidator("1234567891234563")).to.be.equal(true);
   });
-  it("invalid", () => {
+  it("valid: 1234567891234563", () => {
+    expect(lib.cardValidator(1234567891234563)).to.be.equal(true);
+  });
+  it("invalid: '123456789123456a'", () => {
     expect(lib.cardValidator("123456789123456a")).to.be.equal(false);
   });
-  it("invalid", () => {
+  it("invalid: '123456789123456'", () => {
     expect(lib.cardValidator("123456789123456")).to.be.equal(false);
   });
-  it("invalid", () => {
+  it("invalid: 123456789123456", () => {
+    expect(lib.cardValidator(123456789123456)).to.be.equal(false);
+  });
+  it("invalid: 'aaaaaaaaaaaaaaaa'", () => {
     expect(lib.cardValidator("aaaaaaaaaaaaaaaa")).to.be.equal(false);
   });
 });
